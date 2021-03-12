70d6a134389a7d4c4768dbcc0d3fffb8e990fabc
kushagra-pandey <epickush2000@gmail.com>
2021-03-12 01:35:40 -0800

    added partitioning

--- src/test_partition.c
+++ src/test_partition.c
@@ -12,6 +12,10 @@
 #include "include/gqf_int.h"
 #include "include/gqf_file.h"
 
+#define MAX_VALUE(nbits) ((1ULL << (nbits)) - 1)
+#define BITMASK(nbits)                                    \
+  ((nbits) == 64 ? 0xffffffffffffffff : MAX_VALUE(nbits))
+
 int main(int argc, char **argv)
 {
 
